a56ea37a900cb6dd36119f66d104508b2d5ac727
Andrey Komendantov <komandr@gmail.com>
2022-09-26 00:21:16 +0300

    refactor: deleted file .h

--- first.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> #include <omp.h>
-main()
-{
-/* Выделение параллельного фрагмента*/
-#pragma omp parallel
-    {
-        printf("Hello World !\n");
-    } /* Завершение параллельного фрагмента */
-}
