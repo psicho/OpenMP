aaae85445ac08b4b3d09803f6b81c6c7794b97a3
Andrey Komendantov <komandr@gmail.com>
2022-09-26 19:56:03 +0300

    feat: added new task 1 script

---
+++ src/tasks/001.cpp
@@ -0,0 +1,14 @@
+// Задача
+// Разработайте программу для нахождения минимального 
+// (максимального) значения среди элементов вектора.
+
+#include <iostream>
+#include <stdio.h>
+#include <omp.h>
+int main(){
+#pragma omp parallel for
+  for(int i=0;i<100;i++){
+    printf("%i\n",i);
+  }
+  return 0;
+}
