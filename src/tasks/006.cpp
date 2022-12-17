cd14fe7f836bcc99caa71f78b8d6db172ebdc59b
Andrey Komendantov <komandr@gmail.com>
2022-12-17 18:20:55 +0300

    feat: updated task 6

--- src/tasks/006.cpp
+++ src/tasks/006.cpp
@@ -15,31 +15,27 @@ using namespace std;
 int main(){
 
 // int n = 1410065408; // 10**10
-//int n = 1000000000; // 10**9
+int n = 1000000000; // 10**9
 // int n = 100000000; // 10**8
 // int n = 1000000; // 10**6
- int n = 100000; // 10**5
-// int n = 1000; // 10**3
-
-//int *a = (int*) calloc(n, sizeof(int));
-//int *b = (int*) calloc(n, sizeof(int));
-//int *res = (int*) calloc(n, sizeof(int));
-//int *res_par = (int*) calloc(n, sizeof(int));
 
 float h,S,x;
 int i, a, b;
+int chunk = 10;
+int count_treads = 50;
 
 a = 1;
 b = 5;
 h=(b-a) * 1.0 / n;
-S=0;
+S=0.000000;
+float result=0.0000000;
 
 double time_spent = 0.00000000;
 double time_spent_par = 0.00000000;
 
 // Расчет времени при последовательном выполнении
 clock_t begin =  clock();
-for(i=0;i<n-1;i++)
+for(i=0;i<n;++i)
 {
     x=a+i*h;
     S=S+(1+sin(x)/(1+cos(x)));
@@ -50,11 +46,12 @@ time_spent += (double)(end - begin) / (CLOCKS_PER_SEC);
 printf("\nSequential work time is %.10f seconds", time_spent);
 
 // Расчёт времени при параллельном выполнении
+S=0.0;
 clock_t begin_par =  clock();
-//#pragma omp parallel for num_threads(30)
-#pragma omp parallel for num_threads(30) default(shared) \
-schedule(static,chunk) reduction(+:S)
-for(i=0;i<n-1;i++)
+#pragma omp parallel for num_threads(count_treads) reduction(+:S)
+// #pragma omp parallel for default(shared) \
+// schedule(static,chunk) reduction(+:S)
+for(i=0;i<n;++i)
 {
   x=a+i*h;
   S=S+(1+sin(x)/(1+cos(x)));
